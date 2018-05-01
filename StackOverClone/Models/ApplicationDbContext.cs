4d317f160e96925d8f1aa5802adef6bdf4ef08a4
Cameron Anderson and Ian Goodrich <chamburg321@gmail.com and iangoodrich@hotmail.com>
2018-05-01 10:35:44 -0700

    update DbContext

--- StackOverClone/Models/ApplicationDbContext.cs
+++ StackOverClone/Models/ApplicationDbContext.cs
@@ -3,13 +3,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace StackOverClone.Models
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
         }
 
-      
+        public DbSet<Answer> Answers { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=8889;database=StackClone;uid=root;pwd=root;");
+        }
+
+     
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+
+        }
     }
-}
